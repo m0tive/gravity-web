8458606223c1ae59e1c02f2ddc61728285dfc111
Peter Dodds <peter@pddds.com>
2024-09-22 23:39:53 +0100

    Update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,22 @@
-import { Linter } from 'eslint';
+import { configs as tsConfigs } from "@typescript-eslint/eslint-plugin";
+//import { configs as reactConfigs } from "eslint-plugin-react";
+import { configs as jsConfigs } from "@eslint/js";
+import globals from "globals";
 
-const config: Linter.Config = {
-  files: ["**/*.ts", "**/*.tsx"],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020, // Allows parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+export default [
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsConfigs.recommended,
+      //react: reactConfigs.recommended,
+    },
   },
-  extends: [
-    'eslint:recommended', // Use the recommended rules from eslint
-    'plugin:@typescript-eslint/recommended', // Use the recommended rules from @typescript-eslint/eslint-plugin
-    //'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  rules: {
-    // Custom rules can be added here
-  },
-};
-
-export default config;
+  jsConfigs.recommended,
+];
