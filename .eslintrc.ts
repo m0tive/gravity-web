c463fc598cd856cde1b783d28174470d92084b5b
Peter Dodds <peter@pddds.com>
2024-09-22 22:32:47 +0100

    Create .eslintrc.ts

---
+++ .eslintrc.ts
@@ -0,0 +1,19 @@
+import { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  extends: [
+    'eslint:recommended', // Use the recommended rules from eslint
+    'plugin:@typescript-eslint/recommended', // Use the recommended rules from @typescript-eslint/eslint-plugin
+    //'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    // Custom rules can be added here
+  },
+};
+
+export default config;
