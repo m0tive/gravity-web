7b12c586aee8fd6735621ad9c921415d0d066a18
Peter Dodds <peter@pddds.com>
2024-09-23 09:57:53 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Gravity Web</h1>
+      <p>This is a placeholder for the game UI.</p>
+    </div>
+  );
+};
