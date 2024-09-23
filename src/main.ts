cf8ba69a62c7ca13a39c47104a6f5be32706e4c2
Peter Dodds <peter@pddds.com>
2024-09-23 09:55:53 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("gravity wave 👋");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
