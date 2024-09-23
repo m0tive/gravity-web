3f4c717dfca6c59c467d949773a177a4dc2b52ae
Peter Dodds <peter@pddds.com>
2024-09-23 21:43:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/gravity-web/'
+  base: '/gravity-web/',
+  plugins: [
+    react(),
+    viteTsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      '@': '/src' // Adjust the alias as needed
+    }
+  },
+  server: {
+    port: 3000, // Change the port if necessary
+  },
 });
