78783a2702c1e59126e0b1e5eeca7d1a8e3a636f
Peter Dodds <peter@pddds.com>
2024-09-24 21:02:35 +0100

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,14 @@ const config: Config = {
   verbose: true,
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest', // Transforms TypeScript and TSX files using ts-jest
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src'], // Limit tests to the src directory
   moduleNameMapper: {
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js', // Use custom mock for styles
+    '\\.(css|scss)$': 'identity-obj-proxy', // Mock SCSS/CSS imports
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Optional: Jest setup file
 };
 
 export default config;
