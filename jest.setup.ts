6194e8ace2b7cf239cde7c112b5614496a61eae9
Peter Dodds <peter@pddds.com>
2024-09-26 17:18:40 +0100

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,10 +2,12 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 
-global.import.meta = {
-  env: {
-    VITE_BUILD_TIME: 'mocked-build-time',
-    VITE_GIT_HASH: 'mocked-git-hash',
+global.import = {
+  meta: {
+    env: {
+      VITE_BUILD_TIME: 'mocked-build-time',
+      VITE_GIT_HASH: 'mocked-git-hash',
+    },
   },
 };
 
