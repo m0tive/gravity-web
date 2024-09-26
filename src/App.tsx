9510ef959b2e964d3c84fa76daea5d6e6499aa7c
Peter Dodds <peter@pddds.com>
2024-09-26 16:00:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 import { GamePage } from './components/GamePage';
 import styles from './App.module.scss';
 
 export const App: React.FC = () => {
-  const [isGameStarted, setIsGameStarted] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const startGame = () => {
-    setIsGameStarted(true);
-    navigate('/game');
-  };
-
-  const confirmExitGame = () => {
-    return window.confirm('Are you sure you want to leave the game and go back to the landing page?');
-  };
-
-  useEffect(() => {
-    const handlePopState = () => {
-      if (location.pathname === '/game') {
-        if (confirmExitGame()) {
-          setIsGameStarted(false);
-        } else {
-          navigate('/game'); // Stay on the game page if they cancel
-        }
-      }
-    };
-
-    window.addEventListener('hashchange', handlePopState);
-
-    return () => {
-      window.removeEventListener('hashchange', handlePopState);
-    };
-  }, [location.pathname, navigate]);
-
   return (
-    <div className={styles.container}>
-      {isGameStarted ? <GamePage /> : <LandingPage onStart={startGame} />}
-    </div>
+    
+      <div className={styles.container}>
+        <Switch>
+          <Route path="/game" component={GamePage} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
+  
   );
 };
