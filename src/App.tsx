c0603f8e7a7c9a63ab159ad4a54a6c698505a979
Peter Dodds <peter@pddds.com>
2024-09-26 16:38:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 import { GamePage } from './components/GamePage';
 import styles from './App.module.scss';
 
 export const App: React.FC = () => {
   return (
-    
-      <div className={styles.container}>
-        <Switch>
-          <Route path="/game" component={GamePage} />
-          <Route path="/" component={LandingPage} />
-        </Switch>
-      </div>
-  
+    <div className={styles.container}>
+      <Routes>
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
   );
 };
