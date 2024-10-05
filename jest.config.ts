739311fc5be6df86372b623a431ad0ffeb8ba88f
Peter Dodds <peter@pddds.com>
2024-10-05 15:27:03 +0100

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
         diagnostics: {
           ignoreCodes: [1343]
         },
-        astTransformers: {
-          before: [
-            {
-              path: 'node_modules/ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
-              options: { metaObjectReplacement: { env: {} } }
-            }
-          ]
-        }
+        //astTransformers: {
+        //  before: [
+        //    {
+        //      path: 'node_modules/ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+        //      options: { metaObjectReplacement: { env: {} } }
+        //    }
+        //  ]
+        //}
       },
     ],
   },
