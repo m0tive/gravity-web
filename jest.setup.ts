188964c3a7c7a1e051cd935ac3b4e121664edf84
Peter Dodds <peter@pddds.com>
2024-10-05 21:23:01 +0100

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,10 @@
 // jest.setup.ts
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
-import fetch from 'node-fetch';
+import fetch, { Request, Response } from 'node-fetch';
 
-global.Request = fetch.Request;
-global.Response = fetch.Response;
+global.Request = Request;
+global.Response = Response;
 global.fetch = fetch;
 
 //global.import = {
