00cdfa080f69585701f50cde9e6fd4c7d73dfd8a
Peter Dodds <peter@pddds.com>
2024-10-05 21:35:42 +0100

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,9 +4,12 @@ import 'jest-canvas-mock';
 import fetch from 'node-fetch';
 
 // Assigning directly to global without redeclaration
+/* eslint-disable @typescript-eslint/no-explicit-any */
 (global as any).Request = fetch.Request;
 (global as any).Response = fetch.Response;
 (global as any).fetch = fetch;
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
 
 //global.import = {
 //  meta: {
