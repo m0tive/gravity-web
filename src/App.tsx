6793cc3c114098514fac19327824f9e5adf7f87d
Peter Dodds <peter@pddds.com>
2024-10-05 20:44:29 +0100

    Update App.tsx to use router outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { LandingPage } from './components/LandingPage';
-import { GamePage } from './components/GamePage';
+import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
 
 export const App: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Routes>
-        <Route path="/game" element={<GamePage />} />
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      <Outlet />
     </div>
   );
 };
