803e07a8b5589030ed8eef3f422f7f6d26ad0cfa
Peter Dodds <peter@pddds.com>
2024-10-05 20:58:22 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import routes from './routes';
+
+const router = createHashRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
