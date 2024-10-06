895fd2291855f704440c6425dafe6a6240ae7ae7
Peter Dodds <peter@pddds.com>
2024-10-07 00:06:17 +0100

    Update phaserGame.ts fix type error

--- src/phaserGame.ts
+++ src/phaserGame.ts
@@ -14,7 +14,7 @@ export function initializePhaserGame() {
     physics: {
       default: 'matter',
       matter: {
-        gravity: { y: 0.5 },
+        gravity: { x: 0, y: 0.5 },
       },
     },
   });
