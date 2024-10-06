442094ad087ed21d3fcfc72161aa73107f9f4ba7
Peter Dodds <peter@pddds.com>
2024-10-06 22:55:41 +0100

    Create GameCanvas.tsx

---
+++ src/components/GameCanvas.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { initializePhaserGame } from '../phaserGame';
+
+export const GameCanvas: React.FC = () => {
+  useEffect(() => {
+    const phaserGame = initializePhaserGame();
+
+    return () => {
+      // Clean up Phaser when the component unmounts
+      phaserGame.destroy(true);
+    };
+  }, []);
+
+  return <div id="game-container" style={{ width: '100%', height: '100%' }} />;
+};
