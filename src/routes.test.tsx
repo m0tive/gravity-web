ebece51f647ea3bd20779ba7947618abb5a485a0
Peter Dodds <peter@pddds.com>
2024-10-07 21:37:10 +0100

    Update routes.test.tsx lint fix

--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes'; // Import the routes configuration
 
