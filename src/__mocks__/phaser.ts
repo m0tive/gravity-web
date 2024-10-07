1e890074897bbd08a5fc4560e01cc1b5403aa960
Peter Dodds <peter@pddds.com>
2024-10-07 13:50:07 +0100

    Update phaser.ts to fix types

--- src/__mocks__/phaser.ts
+++ src/__mocks__/phaser.ts
@@ -1,10 +1,9 @@
 
-const Phaser = jest.createMockFromModule('phaser');
+const Phaser = jest.createMockFromModule('phaser') as jest.Mocked<typeof import('phaser')>;
 
 // Add custom mock implementations if needed
 Phaser.Game = jest.fn().mockImplementation(() => ({
-  start: jest.fn(),
-  stop: jest.fn(),
+  destroy: jest.fn(),
 }));
 
 export default Phaser;
