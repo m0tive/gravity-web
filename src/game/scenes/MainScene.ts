f70649d68751efef073f098d72f90e43cd4cef0d
Peter Dodds <peter@pddds.com>
2024-10-08 22:19:39 +0100

    Create MainScene.ts

---
+++ src/game/scenes/MainScene.ts
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+import { createGroundTexture } from '../textures/GroundTexture';
+import { createThrusterParticle } from '../textures/ThrusterParticle';
+import { PlayerShip } from '../objects/PlayerShip';
+
+export default class MainScene extends Phaser.Scene {
+  private player!: PlayerShip;
+
+  constructor() {
+    super({ key: 'MainScene' });
+  }
+
+  create() {
+    // Create procedural ground texture
+    const groundTexture = this.textures.createCanvas('ground', 800, 40);
+    createGroundTexture(groundTexture);
+
+    // Add ground to the scene
+    this.add.image(400, 580, 'ground');
+    this.matter.add.rectangle(400, 580, 800, 40, { isStatic: true });
+
+    // Initialize player ship
+    this.player = new PlayerShip(this, 400, 300);
+
+    // Add thruster particle effect
+    createThrusterParticle(this);
+  }
+
+  update(time: number, delta: number) {
+    this.player.update(time, delta);
+  }
+}
