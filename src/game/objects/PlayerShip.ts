17295981feed26b00bf68ca0bd5a1e04cf6a58e5
Peter Dodds <peter@pddds.com>
2024-10-08 22:18:14 +0100

    Create PlayerShip.ts object

---
+++ src/game/objects/PlayerShip.ts
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+
+export class PlayerShip {
+  private sprite: Phaser.Physics.Matter.Image;
+  private scene: Phaser.Scene;
+
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    this.scene = scene;
+    this.sprite = this.scene.matter.add.image(x, y, undefined);
+    this.sprite.setFrictionAir(0.05);
+  }
+
+  update(time: number, delta: number) {
+    const pointer = this.scene.input.activePointer;
+
+    if (pointer.isDown) {
+      this.sprite.thrust(0.05);
+    }
+
+    if (pointer.x < this.sprite.x) {
+      this.sprite.setAngularVelocity(-0.05);
+    } else if (pointer.x > this.sprite.x) {
+      this.sprite.setAngularVelocity(0.05);
+    }
+  }
+}
